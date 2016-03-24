ba62eee3f81585111d04ea633c8bd7c296f7761d
Victor Bucataru <ping@devktor.com>
2016-03-24 06:20:27 +0200

    cpp lib class naming

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -1,9 +1,9 @@
 
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/cpp/BitProfile.hpp.in ${CMAKE_CURRENT_BINARY_DIR}/cpp/include/bitprofile/Contract.hpp)
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/cpp/BitProfile.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/cpp/Contract.cpp)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/cpp/BitProfile.hpp.in ${CMAKE_CURRENT_BINARY_DIR}/cpp/include/bitprofile/ContractCode.hpp)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/cpp/BitProfile.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/cpp/ContractCode.cpp)
 
 include_directories(${CMAKE_CURRENT_BINARY_DIR}/cpp/include/bitprofile)
-add_library(bitprofile-contract STATIC ${CMAKE_CURRENT_BINARY_DIR}/cpp/Contract.cpp)
+add_library(bitprofile-contract STATIC ${CMAKE_CURRENT_BINARY_DIR}/cpp/ContractCode.cpp)
 
 
 install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/cpp/include DESTINATION include)
